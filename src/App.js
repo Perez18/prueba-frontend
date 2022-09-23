123cd6d5a442cc37c561521d5d54e27e16aae827
anthony perez <anthony.perez@segfedpa.com>
2022-09-22 20:57:47 -0500

    instalaciones de librerias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+// import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Containers/Layout';
+import Page404 from './views/pages/Page404';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' name='Layout' element={<Layout />} />
+        <Route exact path='*' name='Not Match' element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
