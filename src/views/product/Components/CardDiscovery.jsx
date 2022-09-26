7ae66958501292b0ea42061988cc80e985656600
anthony perez <anthony.perez@segfedpa.com>
2022-09-25 20:01:57 -0500

    card discovery add imagen

--- src/views/product/Components/CardDiscovery.jsx
+++ src/views/product/Components/CardDiscovery.jsx
@@ -14,7 +14,7 @@ export const CardDiscovery = ({
 }) => {
 
     return (
-        <div className='card-discovery bg-secondary'>
+        <div className='card-discovery'>
             <Row>
                 {
                     imageData.map(({ src, alt, isImage, isArticle, article }, index) => (
@@ -23,7 +23,7 @@ export const CardDiscovery = ({
                             sm={6}
                             md={4}
                             lg={3}
-                            className={`${isArticle ? article?.background : ''} pt-1 pr-1`}
+                            className='content'
                         >
                             {
                                 isImage
@@ -31,7 +31,8 @@ export const CardDiscovery = ({
                                     <Image
                                         src={src}
                                         alt={alt}
-                                        fluid
+                                        className='image'
+                                        // fluid
                                     />
                                 )
                             }
@@ -39,7 +40,7 @@ export const CardDiscovery = ({
                             {
                                 isArticle
                                 && (
-                                    <div className='article'>
+                                    <div className={`article ${article?.background}`}>
                                         <h3 className='title'>{article?.title}</h3>
                                         <p className='text'>{article?.text}</p>
                                         <p className='more'>{article?.helper} <FontAwesomeIcon icon={faArrowRight} /> </p>
@@ -51,11 +52,7 @@ export const CardDiscovery = ({
                             {
                                 (!isImage && !isArticle)
                                 && (
-                                    <Image
-                                        src='data:image/svg+xml;charset=UTF-8,<svg%20width%3D"285"%20height%3D"220"%20xmlns%3D"http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg"%20viewBox%3D"0%200%20243%20160"%20preserveAspectRatio%3D"none"><defs><style%20type%3D"text%2Fcss">%23holder_1836dca6e78%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A12pt%20%7D%20<%2Fstyle><%2Fdefs><g%20id%3D"holder_1836dca6e78"><rect%20width%3D"243"%20height%3D"160"%20fill%3D"%23373940"><%2Frect><g><text%20x%3D"91.765625"%20y%3D"86.3">243x160<%2Ftext><%2Fg><%2Fg><%2Fsvg>'
-                                        alt={alt}
-                                        fluid
-                                    />
+                                    <div className='d-block bg-block-gray'></div>
                                 )
                             }
                         </Col>
@@ -63,7 +60,7 @@ export const CardDiscovery = ({
 
                 }
             </Row>
-        </div>
+        </div >
     )
 }
 
@@ -80,7 +77,7 @@ CardDiscovery.defaultProps = {
             article: {
                 background: 'bg-yellow-light',
                 title: 'Discover your true style',
-                text: 'Penatibus et magnis dis montes nascetur ridiculus mus.',
+                text: 'Penatibus et magnis dis montes nascetur ridiculus mus. Magna nulla quis aliqua duis aliqua consectetur.',
                 helper: 'explore our trends'
             }
         },
--- src/views/product/Components/CardShop.jsx
+++ src/views/product/Components/CardShop.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import { Col, Row,Image } from 'react-bootstrap'
+import { Col, Row, Image } from 'react-bootstrap'
 
 // img/icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-
-import shop from 'src/assets/images/shop.webp'
 
 export const CardShop = () => {
   return (
@@ -16,9 +14,7 @@ export const CardShop = () => {
           sm={6}
           md={4}
           lg={8}
-          className='content-image p-0'>
-          {/* <Image src={shop} /> */}
-          {/* <h1>Holas</h1> */}
+          className='p-0'>
           <div className='image'></div>
         </Col>
         <Col
@@ -26,15 +22,16 @@ export const CardShop = () => {
           md={4}
           lg={4}
           className='article'>
-            <h2>Shop anything</h2>
-            <h2>and everything</h2>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-            </p>
-            <p className='more'>
-              browse our collections <FontAwesomeIcon icon={faArrowRight} />
-            </p>
+          <h2>Shop anything</h2>
+          <h2>and everything</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            lorem
+          </p>
+          <p className='more'>
+            browse our collections <FontAwesomeIcon icon={faArrowRight} />
+          </p>
         </Col>
       </Row>
     </div>
